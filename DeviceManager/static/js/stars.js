ba7d8f47dd1a90953b7480cf43efd814e88df15e
Adrian-Nicolae Dediu <111912660+adrdediu@users.noreply.github.com>
2024-08-14 18:04:51 +0300

    Updates: - Links to external CSS and JS are now static files

--- DeviceManager/static/js/stars.js
+++ DeviceManager/static/js/stars.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+import * as THREE from './three.module.js';
 
 // Get the homepageDisplayArea and the canvas
 var homepageDisplayArea = document.getElementById('homepageDisplayArea');
